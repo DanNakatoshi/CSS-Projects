4a22849134838a16873f188e3a8a4df499d5f0e8
NeatPeople <61215328+NeatPeople@users.noreply.github.com>
2021-06-16 17:35:56 -0400

    css08 Wave Effect

---
+++ css07 スクロールアニメーション/script.js
@@ -0,0 +1,24 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+
+checkBoxes();
+
+function checkBoxes() {
+  // console.log(window.innerHeight / 5*4);
+  const triggerBottom =   window.innerHeight / 5 * 4;
+
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+    // コンテンツボックスのトップの高さがトリガーボトムと同じになった場合にアニメーションを起動する
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+
+  })
+
+}
